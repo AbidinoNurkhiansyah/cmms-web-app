c81fe2343f68f43dfe94936e0521457fb6c78502
Abidino Nurkhiansyah <abidinonurkhiansyah@gmail.com>
2026-07-06 23:57:36 +0700

    refactor(stock-taking): clean up unused modal traits and implement maryUI header

--- resources/views/livewire/spare-part/stock-taking/index.blade.php
+++ resources/views/livewire/spare-part/stock-taking/index.blade.php
@@ -1,28 +1,15 @@
 <?php
 
 use App\Services\SparePartStockTakingService;
-use App\Models\SparePart;
 use Livewire\Volt\Component;
 use Livewire\WithPagination;
 use Mary\Traits\Toast;
-use App\Livewire\SparePart\StockTaking\Traits\WithStockTakingModals;
 use Livewire\Attributes\Computed;
 
 new class extends Component {
-    use WithPagination, Toast, WithStockTakingModals;
+    use WithPagination, Toast;
 
     public string $search = '';
-    
-    public function mount()
-    {
-        $this->mountWithStockTakingModals();
-    }
-
-    #[Computed]
-    public function spareParts()
-    {
-        return SparePart::select('id', 'part_number', 'part_name', 'price_idr', 'last_stock')->get();
-    }
 
     public function updatedSearch(): void
     {
@@ -46,5 +33,4 @@ new class extends Component {
 <div>
     @include('livewire.spare-part.stock-taking.partials.header')
     @include('livewire.spare-part.stock-taking.partials.table')
-    @include('livewire.spare-part.stock-taking.partials.modal-form')
 </div>
--- resources/views/livewire/spare-part/stock-taking/partials/header.blade.php
+++ resources/views/livewire/spare-part/stock-taking/partials/header.blade.php
@@ -1,24 +1,12 @@
-<div class="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-4 mb-6">
-    <div class="flex items-center gap-2">
-        <x-button icon="o-plus" class="btn-primary btn-sm" wire:click="createStockTaking" spinner>
-            New Record
-        </x-button>
-    </div>
-    
-    <div class="flex w-full sm:w-auto items-center gap-2">
-        <x-input 
-            icon="o-magnifying-glass" 
-            placeholder="Search Date (YYYY-MM-DD)..." 
-            wire:model.live.debounce.300ms="search" 
-            class="input-sm w-full sm:w-64"
-            clearable
-        />
-        <x-button 
-            icon="o-x-mark" 
-            wire:click="resetFilters" 
-            class="btn-sm btn-ghost" 
-            tooltip="Reset Filter"
-            spinner
-        />
-    </div>
-</div>
+<x-header title="Stock Taking" separator>
+    <x-slot:actions>
+        <div class="flex w-full sm:w-auto items-center gap-2">
+            <x-input 
+                type="date"
+                wire:model.live="search" 
+                class="input-sm w-full sm:w-48"
+                clearable
+            />
+        </div>
+    </x-slot:actions>
+</x-header>
