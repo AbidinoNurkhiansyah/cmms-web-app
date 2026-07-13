74d5a598e4e1c64de4ea922f6bd3b125a714d33d
Abidino Nurkhiansyah <abidinonurkhiansyah@gmail.com>
2026-07-13 21:42:56 +0700

    feat(user): add line_name column to users table for area restriction

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'line_name',
         'password',
         'jid_no',
         'username',
